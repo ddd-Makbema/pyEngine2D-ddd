cc8f01304f87344b330c1ac863c05974e4e17be2
ddd-Makbema <99289599+ddd-Makbema@users.noreply.github.com>
2023-02-09 09:05:03 -0600

    colliders are better

--- variables.py
+++ variables.py
@@ -15,10 +15,9 @@ GO.GAME_OBJECTS["sam"].Transform.scale = [1,1]
 GO.GAME_OBJECTS["sam"].Transform.rotation = 0
 
 GO("katz", screen.screen, "null.png", "origin", "transform.Transform", "Collider2D.RectangleCollider2D", "player_movement.PlayerMovement")
-GO("katz", screen.screen, "null.png", "origin", "transform.Transform", "Collider2D.RectangleCollider2D", "player_movement.PlayerMovement")
 GO.GAME_OBJECTS["katz"].RectangleCollider2D.set_collider_size([20,20])
-GO.GAME_OBJECTS["katz"].Transform.transform = [100,100]
-GO.GAME_OBJECTS["katz"].Transform.scale = [2,2]
+GO.GAME_OBJECTS["katz"].Transform.transform = [300,250]
+GO.GAME_OBJECTS["katz"].Transform.scale = [1,1]
 GO.GAME_OBJECTS["katz"].Transform.rotation = 0
 
 if __name__ == '__main__':
