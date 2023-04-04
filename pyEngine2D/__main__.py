ce55479cb59f3586f3102ba9f4b68bea435b868b
ddd-Makbema <99289599+ddd-Makbema@users.noreply.github.com>
2023-04-04 08:43:56 -0500

    changing src to pyEngine2D

---
+++ pyEngine2D/__main__.py
@@ -0,0 +1,92 @@
+import pygame
+import time
+from src.game_object import GameObject
+from src.variables import screen
+
+running = True
+timer = 0
+start_time = time.time()
+
+
+list_load_game_object = []
+
+
+
+def update():
+	"""Renders the screen fps times a second."""
+	screen.screen.fill(screen.green)
+	for event in pygame.event.get():   
+		if event.type == pygame.QUIT:
+			running = False
+		if event.type == pygame.VIDEORESIZE:
+			screen.width = screen.screen.get_size()[0]
+			screen.height = screen.screen.get_size()[1]
+			print([screen.width, screen.height])
+	for object in GameObject.INSTANCES:
+		object.update()			
+	#loads the images onto the screen accounting for transform
+	for object in GameObject.INSTANCES:
+		try:
+			object_transform = object.Transform.load.get_rect(
+				center = object.Transform.parent_transform())
+			screen.screen.blit(object.Transform.load, object_transform)
+		except AttributeError as e:
+			if "no attribute 'Transform'" not in str(e):
+				raise e
+			else:
+				center = [-1,-1]
+				screen.screen.blit(object.image_start, center)
+	screen.frame_end()  
+
+def fixed_update():
+	"""calls the fixed update function at set times per second. Independent from update"""
+	for object in GameObject.INSTANCES:
+		object.fixed_update()
+
+def delta_time():
+	global start_time
+	x = time.time() - start_time
+	start_time = time.time()
+	return x
+
+def game_loop():
+	"""The main game loop that runs as the game runs. Returns when the pygame window is closed."""
+	global running
+	global timer
+	for object in GameObject.INSTANCES:
+		object.start()
+	while running:
+		for event in pygame.event.get():  
+			if event.type == pygame.QUIT:
+				running = False
+				return
+			if event.type == pygame.VIDEORESIZE:
+				screen.width = screen.screen.get_size()[0]
+				screen.height = screen.screen.get_size()[1]
+				print([screen.width, screen.height])
+		while timer > screen.fixed_fps:
+			fixed_update()
+			timer -= screen.fixed_fps
+		update()
+		screen.clock.tick(screen.fps)
+		timer += delta_time()
+	pygame.quit()
+	return
+	
+game_loop()
+
+pygame.quit()
+
+if __name__ == '__main__':
+    pass
+
+# All Awake calls
+# All Start Calls
+# while (stepping towards variable delta time)
+# All FixedUpdate functions
+# Physics simulation
+# OnEnter/Exit/Stay trigger functions
+# OnEnter/Exit/Stay collision functions
+# Rigidbody interpolation applies transform.position and rotation
+# OnMouseDown/OnMouseUp etc. events
+# All Update functions
---
+++ pyEngine2D/scripts/user/player_movement.py
@@ -0,0 +1,17 @@
+from src.game_object import GameObject
+import pygame
+
+class PlayerMovement:
+	def __init__(self, game_object):
+		self.game_object = game_object
+	def fixed_update(self):
+		# self.game_object.Transform.transform = self.game_object.Transform.transform + [1,1]
+		self.game_object.Transform.rotation = self.game_object.Transform.rotation -1
+		# self.game_object.Transform.rotation = 45
+		# self.game_object.Transform.scale = [self.game_object.Transform.scale[0] * 0.99, self.game_object.Transform.scale[1] * 0.999]
+		pass
+	def update(self):
+		pass
+
+if __name__ == '__main__':
+    pass
